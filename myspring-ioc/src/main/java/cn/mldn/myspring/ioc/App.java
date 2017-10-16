a168b01519997593f41b51e69e28b577986e4287
zj <531847459@qq.com>
2017-10-16 22:07:12 +0800

    测试使用

---
+++ myspring-ioc/src/main/java/cn/mldn/myspring/ioc/App.java
@@ -0,0 +1,13 @@
+package cn.mldn.myspring.ioc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worldzzzzs!" );
+    }
+}
