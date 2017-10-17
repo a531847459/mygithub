87e3d78068f0e10535319e40e652685f71b3bafc
zj <531847459@qq.com>
2017-10-18 00:27:46 +0800

    基于Spring配置文件的SpringTask任务调度配置

--- myspring-schdule/src/main/java/cn/mldn/myspring/schdule/quartz/MyTaskSpring.java
+++ myspring-schdule/src/main/java/cn/mldn/myspring/schdule/quartz/MyTaskSpring.java
@@ -2,6 +2,8 @@ package cn.mldn.myspring.schdule.quartz;
 
 import java.text.SimpleDateFormat;
 
+import org.springframework.stereotype.Component;
+//@Component
 public class MyTaskSpring {
 	public void myTask() {
 		System.out.println("【 当前时间】"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date()));
